4fb5108d14f6876c3ffd3faa941110dc684b87a3
meetdhamecha123 <dhamechameet104@gmail.com>
2026-03-22 12:03:11 +0530

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/present-my-awesome-self">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
